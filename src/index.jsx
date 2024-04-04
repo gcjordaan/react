edf97fedffe102c0375010192bfd00dbae3fb0a0
Gideon Jordaan <gidz.jordaan@gmail.com>
2024-04-04 16:16:49 +0200

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const fName = "Gideon";
+const lName = "Jordaan";
+const num = 27;
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {fName + " " + lName}!</h1>
+    <p>Your lucky number is {num}</p>
+  </div>,
+  document.getElementById("root")
+);
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
